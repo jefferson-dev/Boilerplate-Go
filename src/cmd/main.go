99ccd50c3c39e03660db8ddacc5fd3893c4109c2
Jefferson Neres <jefferson.neres@mutual.club>
2022-04-01 12:40:47 -0300

    corrections and insert ambient variables

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+
 	"boilerplate/src/infra/database"
 	"boilerplate/src/infra/routes"
 	"log"
 )
 
 func main() {
-	database.DbConnection()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	database.InitDB()
 
 	app := routes.Routes()
 
--- src/infra/database/database.go
+++ src/infra/database/database.go
@@ -2,20 +2,33 @@ package database
 
 import (
 	"boilerplate/src/modules/user/domain/entity"
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
-	DB  *gorm.DB
-	err error
+	DB *gorm.DB
 )
 
-func DbConnection() {
-	dsn := "host=localhost user=postgres password=123456 dbname=database port=5432 sslmode=disable TimeZone=America/Fortaleza"
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func InitDB() {
+	connectionString := "host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s"
+
+	dsn := fmt.Sprintf(
+		connectionString,
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("SSL_MODE"),
+		os.Getenv("TIMEZONE"),
+	)
+
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Panic("Error Connection Database")
